bb21a268c807947a6d686daa035177e6c2e0a18d
Bob McMichael <rmcmichael@nist.gov>
2019-06-27 10:27:21 -0400

    exposes Socket class for distro in __init__.py

--- optbayesexpt/__init__.py
+++ optbayesexpt/__init__.py
@@ -3,3 +3,4 @@ from .ProbDistFunc_class import ProbDistFunc
 from .ExptModel_class import ExptModel
 from .OptBayesExpt import OptBayesExpt
 from .OBETCP import OBE_Server
+from .OBETCP import Socket
