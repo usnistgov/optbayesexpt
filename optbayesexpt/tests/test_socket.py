0739e97880bd72c12e696950dfd32051809c2171
Bob McMichael <rmcmichael@nist.gov>
2019-07-22 12:10:19 -0400

    adds testing of TCP communications

--- optbayesexpt/tests/test_socket.py
+++ optbayesexpt/tests/test_socket.py
@@ -3,8 +3,9 @@ from numpy.testing import assert_array_equal
 
 from optbayesexpt import Socket
 
-myClient = Socket('client')
-myServer = Socket('server')
+
+myClient = Socket('client', port=60899)
+myServer = Socket('server', port=60899)
 
 def test_send_recieve_text():
     contents = 'test message'
@@ -27,6 +28,18 @@ def test_send_recieve_array():
     myClient.close()
     assert received == contents
 
+def test_send_receive_dict():
+    contents = {'message': 'Howdy!', 'pi': 3.1415, 'count': [1, 2, 3]}
+    myClient.send(contents)
+    received = myServer.receive()
+    myClient.close()
+    assert received == contents
+
 """
-The tcpcmd method is tested in test_obe_server.py
+The tcpcmd() method is tested in test_server.py
 """
+if __name__ == '__main__':
+    test_send_recieve_text()
+    test_send_recieve_number()
+    test_send_recieve_array()
+    test_send_receive_dict()
---
+++ optbayesexpt/tests/test_sz_server.py
@@ -0,0 +1,75 @@
+
+import numpy as np
+import optbayesexpt as obemod
+from numpy.testing import assert_array_equal
+
+from time import sleep
+from subprocess import Popen
+import os
+
+# This test focuses on testing communications with an OBE server
+
+
+server_script = os.path.join(os.getcwd(), "optbayesexpt/tests/server_script.py")
+server_pipe = Popen(['python', server_script])
+sleep(3)
+
+
+# start up a socket to communicate with server
+obe = obemod.Socket(role='client', port=61982)
+
+# settings
+s1 = np.array([1, 2, 3], dtype='float')
+s2 = np.array([4, 5, 6], dtype='float')
+
+# parameters
+p1 = (11, 12, 13)
+p2 = [14, 15, 16]
+
+# constants
+cc = [101, 102, 103.1]
+
+
+def test_addset():
+    assert 'OK' == obe.tcpcmd({'command': 'addset', 'array': s1.tolist()})
+    assert 'OK' == obe.tcpcmd({'command': 'addset', 'array': s2.tolist()})
+
+
+def test_addpar():
+    assert 'OK' == obe.tcpcmd({'command': 'addpar', 'array': p1})
+    assert 'OK' == obe.tcpcmd({'command': 'addpar', 'array': p2})
+
+
+def test_addcon():
+    for value in cc:
+        assert 'OK' == obe.tcpcmd({'command': 'addcon', 'value': value})
+
+
+def test_config():
+    assert 'OK' == obe.tcpcmd({'command': 'config'})
+
+
+def test_getset():
+    reply = obe.tcpcmd({'command': 'getset'})
+    assert_array_equal(reply, [s1, s2])
+
+
+def test_getpar():
+    reply = obe.tcpcmd({'command': 'getpar'})
+    assert_array_equal(reply, [p1, p2])
+
+
+def test_getcon():
+    reply = obe.tcpcmd({'command': 'getcon'})
+    assert_array_equal(reply, cc)
+
+
+def test_done():
+    assert 'OK' == obe.tcpcmd({'command': 'done'})
+
+
+if __name__ == '__main__':
+    test_addset()
+    test_config()
+    test_getset()
+    test_done()
