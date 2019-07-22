f9c5ea0d3095e803ae3bce13b29be38d6be78250
Bob McMichael <rmcmichael@nist.gov>
2019-07-22 16:03:21 -0400

    trims distro to source code

--- build/lib/optbayesexpt/__init__.py
+++
@@ -1,6 +0,0 @@
-
-from .ExptModel_class import ExptModel
-from .obe_socket import Socket
-from .obe_server import OBE_Server
-from .OptBayesExpt import OptBayesExpt
-from .ProbDistFunc_class import ProbDistFunc
