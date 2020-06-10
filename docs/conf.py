1ecc2aace549950540edb33387cd5808789a9cf1
Bob McMichael <rdm@nist.gov>
2020-06-10 13:54:58 -0400

    Changes version to 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = 'National Institute of Standards and Technology.' \
 author = 'Bob McMichael'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
 
