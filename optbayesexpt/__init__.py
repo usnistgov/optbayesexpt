d0de045944e57cc848453133bcd3c8aa8b7f883c
Bob McMichael <rdm@nist.gov>
2021-05-18 17:56:49 -0400

    adds obe_noiseparam w/ OptBayesExptNoiseParameter to package

--- optbayesexpt/__init__.py
+++ optbayesexpt/__init__.py
@@ -4,3 +4,4 @@ from .obe_server import OBE_Server
 from .obe_base import OptBayesExpt
 from .particlepdf import ParticlePDF
 from .obe_utils import MeasurementSimulator, trace_sort
+from .obe_noiseparam import OptBayesExptNoiseParameter
