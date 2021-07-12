622a7ba2524a0e540615ff1a5b2adfa3ebce7619
Bob McMichael <rdm@nist.gov>
2021-07-12 16:23:28 -0400

    rearranges order of import statements

--- optbayesexpt/__init__.py
+++ optbayesexpt/__init__.py
@@ -1,8 +1,8 @@
-
+from .constants import GOT_NUMBA, optbayesexpt_version
+from .particlepdf import ParticlePDF
+from .obe_base import OptBayesExpt
+from .obe_noiseparam import OptBayesExptNoiseParameter
 from .obe_socket import Socket
 from .obe_server import OBE_Server
-from .obe_base import OptBayesExpt
-from .particlepdf import ParticlePDF
 from .obe_utils import MeasurementSimulator, trace_sort
-from .obe_noiseparam import OptBayesExptNoiseParameter
-from .constants import GOT_NUMBA, optbayesexpt_version
+
