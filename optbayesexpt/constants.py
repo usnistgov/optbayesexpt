5d6f5dd2bfa3740656985f90fd96b384567580c3
Bob McMichael <rdm@nist.gov>
2022-12-22 10:13:04 -0500

    adds GOT_SCIPY constant

--- optbayesexpt/constants.py
+++ optbayesexpt/constants.py
@@ -13,8 +13,21 @@ if sys.version_info[0] == 3:
     if numba_module is not None:
         GOT_NUMBA = True
 
+GOT_SCIPY = False
+if sys.version_info[0] == 3:
+    if sys.version_info[1] <= 3:
+        scipy_module = importlib.import_loader('scipy')
+    if sys.version_info[1] >= 4:
+        scipy_module = importlib.util.find_spec('scipy')
+
+    if scipy_module is not None:
+        GOT_SCIPY = True
+
 if __name__ == '__main__':
     print(f'version = {version}')
     print(f'GOT_NUMBA = {GOT_NUMBA}')
     if GOT_NUMBA:
         print(numba_module)
+    print(f'GOT_SCIPY = {GOT_SCIPY}')
+    if GOT_SCIPY:
+        print(scipy_module)
