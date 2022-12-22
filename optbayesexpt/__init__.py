458aa163da445aa2986ecae035d99289cfeb3ce4
Bob McMichael <rdm@nist.gov>
2022-12-22 10:15:22 -0500

    prioritizes scipy.stats version of differential_entropy()

--- optbayesexpt/__init__.py
+++ optbayesexpt/__init__.py
@@ -1,9 +1,17 @@
-import optbayesexpt.constants as constants
-__version__ = constants.version
-GOT_NUMBA = constants.GOT_NUMBA
+from optbayesexpt.constants import version as __version__
+from optbayesexpt.constants import GOT_NUMBA
 from optbayesexpt.particlepdf import ParticlePDF
 from optbayesexpt.obe_base import OptBayesExpt
 from optbayesexpt.obe_noiseparam import OptBayesExptNoiseParameter
 from optbayesexpt.obe_socket import Socket
 from optbayesexpt.obe_server import OBE_Server
-from optbayesexpt.obe_utils import MeasurementSimulator, trace_sort, differential_entropy
+from optbayesexpt.obe_utils import MeasurementSimulator, trace_sort
+
+try:
+    # Try loading differential_entropy from scipy.stats
+    from scipy.stats import differential_entropy
+    print('scipy.stats.differential_entropy')
+except ImportError:
+    # If there are problems, use the version included in obe_utils.py
+    from optbayesexpt.obe_utils import differential_entropy
+    print('obe_utils.differential_entropy')
