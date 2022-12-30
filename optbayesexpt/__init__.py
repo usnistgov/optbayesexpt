2b71587011ae8a38f78523dc8801555ac9e4c07b
Bob McMichael <rdm@nist.gov>
2022-12-30 16:32:17 -0500

    depricates GOT_NUMBA and GOT_SCIPY and adds package metadata

--- optbayesexpt/__init__.py
+++ optbayesexpt/__init__.py
@@ -1,5 +1,3 @@
-from optbayesexpt.constants import version as __version__
-from optbayesexpt.constants import GOT_NUMBA
 from optbayesexpt.particlepdf import ParticlePDF
 from optbayesexpt.obe_base import OptBayesExpt
 from optbayesexpt.obe_noiseparam import OptBayesExptNoiseParameter
@@ -7,10 +5,7 @@ from optbayesexpt.obe_socket import Socket
 from optbayesexpt.obe_server import OBE_Server
 from optbayesexpt.obe_utils import MeasurementSimulator, trace_sort
 
-try:
-    # Try loading differential_entropy from scipy.stats
-    from scipy.stats import differential_entropy
-except ImportError:
-    # If there are problems, use the version included in obe_utils.py
-    from optbayesexpt.obe_utils import differential_entropy
-
+from optbayesexpt.constants import version as __version__
+from optbayesexpt.constants import author as __author__
+from optbayesexpt.constants import author_email as __author_email__
+from optbayesexpt.constants import url as __url__
--- optbayesexpt/constants.py
+++ optbayesexpt/constants.py
@@ -1,33 +1,7 @@
-import sys, importlib
+# package information
+name = 'optbayesexpt'
 version = '1.2.0'
+author = 'Bob McMichael'
+url = 'https://github.com/usnistgov/optbayesexpt/',
+author_email = 'rmcmichael@nist.gov',
 
-# check whether numba is available to load
-# https://stackoverflow.com/questions/14050281/how-to-check-if-a-python-module-exists-without-importing-it
-GOT_NUMBA = False
-if sys.version_info[0] == 3:
-    if sys.version_info[1] <= 3:
-        numba_module = importlib.import_loader('numba')
-    if sys.version_info[1] >= 4:
-        numba_module = importlib.util.find_spec('numba')
-
-    if numba_module is not None:
-        GOT_NUMBA = True
-
-GOT_SCIPY = False
-if sys.version_info[0] == 3:
-    if sys.version_info[1] <= 3:
-        scipy_module = importlib.import_loader('scipy')
-    if sys.version_info[1] >= 4:
-        scipy_module = importlib.util.find_spec('scipy')
-
-    if scipy_module is not None:
-        GOT_SCIPY = True
-
-if __name__ == '__main__':
-    print(f'version = {version}')
-    print(f'GOT_NUMBA = {GOT_NUMBA}')
-    if GOT_NUMBA:
-        print(numba_module)
-    print(f'GOT_SCIPY = {GOT_SCIPY}')
-    if GOT_SCIPY:
-        print(scipy_module)
